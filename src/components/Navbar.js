171fb48a9cb69627d139c04a0d40fc9ff5f3d439
Mayank Mehra <96831095+MayankMehra7@users.noreply.github.com>
2024-09-26 20:44:52 +0530

    push the code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,63 +1,55 @@
-import React, {useState} from 'react'
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
-import Container from 'react-bootstrap/Container'
-import Button from 'react-bootstrap/Button'
-import {Link} from 'react-router-dom'
-import {CgGitFork} from 'react-icons/cg'
+import React, { useState } from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import { Link, useLocation } from 'react-router-dom';
+import { CgFileDocument } from 'react-icons/cg';
 import { FaBriefcase } from "react-icons/fa";
-import {
-  AiFillStar,
-  AiOutlineHome,
-  AiOutlineFundProjectionScreen,
-  AiOutlineUser,
-  AiFillDashboard,
-} from 'react-icons/ai'
-import {  } from "react-icons/si";
-import {CgFileDocument} from 'react-icons/cg'
+import { AiOutlineHome, AiOutlineFundProjectionScreen, AiOutlineUser } from 'react-icons/ai';
 
 function NavBar() {
-  const [expand, updateExpanded] = useState(false)
-  const [navColour, updateNavbar] = useState(false)
+  const [expand, updateExpanded] = useState(false);
+  const [navColour, updateNavbar] = useState(false);
+  
+  // useLocation hook to get the current path
+  const location = useLocation();
 
   function scrollHandler() {
     if (window.scrollY >= 20) {
-      updateNavbar(true)
+      updateNavbar(true);
     } else {
-      updateNavbar(false)
+      updateNavbar(false);
     }
   }
 
-  window.addEventListener('scroll', scrollHandler)
+  window.addEventListener('scroll', scrollHandler);
 
   return (
     <Navbar
       expanded={expand}
       fixed="top"
       expand="md"
-      className={navColour ? 'sticky' : 'navbar'}
+      className={navColour ? 'sticky' : ''}
     >
-      <Container>
-        <Navbar.Brand as={Link} to="/">
+
+<Navbar.Brand as={Link} to="/">
           <p
             style={{
               color: '#fff',
-              margin: 'left',
+             
               fontWeight: '700',
               letterSpacing: '1px',
               fontSize: '28px',
-             
-              
             }}
-          >  
-        Mayank Mehra
-        
+          >
+            Mayank Mehra
           </p>
-        </Navbar.Brand>
+</Navbar.Brand>
+      <Container>
         <Navbar.Toggle
           aria-controls="responsive-navbar-nav"
           onClick={() => {
-            updateExpanded(expand ? false : 'expanded')
+            updateExpanded(expand ? false : 'expanded');
           }}
         >
           <span></span>
@@ -67,8 +59,13 @@ function NavBar() {
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ml-auto" defaultActiveKey="#home">
             <Nav.Item>
-              <Nav.Link as={Link} to="/" onClick={() => updateExpanded(false)}>
-                <AiOutlineHome style={{marginBottom: '2px'}} /> Home
+              <Nav.Link
+                as={Link}
+                to="/"
+                onClick={() => updateExpanded(false)}
+                className={location.pathname === '/' ? 'active' : ''}
+              >
+                <AiOutlineHome style={{ marginBottom: '2px' }} /> Home
               </Nav.Link>
             </Nav.Item>
 
@@ -77,8 +74,9 @@ function NavBar() {
                 as={Link}
                 to="/about"
                 onClick={() => updateExpanded(false)}
+                className={location.pathname === '/about' ? 'active' : ''}
               >
-                <AiOutlineUser style={{marginBottom: '2px'}} /> About
+                <AiOutlineUser style={{ marginBottom: '2px' }} /> About
               </Nav.Link>
             </Nav.Item>
 
@@ -87,9 +85,9 @@ function NavBar() {
                 as={Link}
                 to="/project"
                 onClick={() => updateExpanded(false)}
+                className={location.pathname === '/project' ? 'active' : ''}
               >
-                <AiOutlineFundProjectionScreen style={{marginBottom: '2px'}} />{' '}
-                Projects
+                <AiOutlineFundProjectionScreen style={{ marginBottom: '2px' }} /> Projects
               </Nav.Link>
             </Nav.Item>
 
@@ -98,9 +96,9 @@ function NavBar() {
                 as={Link}
                 to="/Experience"
                 onClick={() => updateExpanded(false)}
+                className={location.pathname === '/Experience' ? 'active' : ''}
               >
-                <FaBriefcase style={{marginBottom: '2px'}} /> {' '}
-                Experience
+                <FaBriefcase style={{ marginBottom: '2px' }} /> Experience
               </Nav.Link>
             </Nav.Item>
 
@@ -109,19 +107,16 @@ function NavBar() {
                 as={Link}
                 to="/resume"
                 onClick={() => updateExpanded(false)}
+                className={location.pathname === '/resume' ? 'active' : ''}
               >
-                <CgFileDocument style={{marginBottom: '2px'}} /> Resume
+                <CgFileDocument style={{ marginBottom: '2px' }} /> Resume
               </Nav.Link>
             </Nav.Item>
-
-
-
-
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
