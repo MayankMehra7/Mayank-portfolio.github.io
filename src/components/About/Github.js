7d6296db536dfc438fa347be36e78e579f556e54
Mayank Mehra <96831095+MayankMehra7@users.noreply.github.com>
2024-10-13 02:17:35 +0530

    changes in npm

--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -6,42 +6,57 @@ function Github() {
   return (
     <Row
       style={{
-        justifyContent: "center", 
-        paddingBottom: "10px", 
-        display: "flex", 
-        flexDirection: "column", 
-        alignItems: "center",  // Aligns items in the center
-        textAlign: "center",   // Centers text inside
-        position: "relative",   // Add this to establish a positioning context
+        justifyContent: "center",
+        paddingBottom: "10px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        textAlign: "center",
+        position: "relative",
+        // height: "100vh", // Set the height to occupy full viewport height
       }}
     >
-      {/* Wrapping only the text in an anchor tag */}
       <h1 className="project-heading" style={{ paddingBottom: "20px" }}>
         <a
           href="https://github.com/MayankMehra7"
           target="_blank"
           rel="noopener noreferrer"
           style={{
-            textDecoration: "none", 
+            textDecoration: "none",
             color: "inherit",
-            cursor: "pointer", // Ensures the link is clickable
-            zIndex: 10, // Ensures the link is on top of other elements
-            display: "inline-block", // Ensures the anchor takes only the space of the text
-            position: "relative", // Helps in positioning if needed
+            cursor: "pointer",
+            zIndex: 10,
+            display: "inline-block",
+            position: "relative",
           }}
         >
           Days I <strong className="purple">Code</strong>
         </a>
       </h1>
 
-      {/* GitHub calendar (clickable part) */}
-      <GitHubCalendar
-        username="MayankMehra7"
-        blockSize={15}
-        blockMargin={5}
-        color="#c084f5"
-        fontSize={16}
-      />
+      {/* Center the GitHub calendar at the bottom */}
+      <div style={{display: "flex", justifyContent: "center" }}>
+        <a
+          href="https://github.com/MayankMehra7"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{
+            textDecoration: "none",
+            color: "inherit",
+            cursor: "pointer",
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <GitHubCalendar
+            username="MayankMehra7"
+            blockSize={15}
+            blockMargin={5}
+            color="#c084f5"
+            fontSize={16}
+          />
+        </a>
+      </div>
     </Row>
   );
 }
